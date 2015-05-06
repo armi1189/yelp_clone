078187e6260f261fe2f68cb224c7a764863181d1
armi1189 <armi1189@gmail.com>
2015-05-06 13:15:51 +0100

    user cannot add restaurants if is not logged in

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -3,7 +3,16 @@ require 'rails_helper'
 feature 'reviewing' do
   before { Restaurant.create name: 'KFC' }
 
-  scenario 'allows users to leave a review using a form' do
+  before do
+        visit '/'
+        click_link 'Sign up'
+        fill_in('Email', with: 'test@example.com')
+        fill_in('Password', with: 'testtest')
+        fill_in('Password confirmation', with: 'testtest')
+        click_button('Sign up')
+  end
+
+  scenario 'allows logged in users to leave a review using a form' do
     visit '/restaurants'
     click_link 'Review KFC'
     fill_in 'Thoughts', with: 'so so'
